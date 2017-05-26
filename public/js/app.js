ddf73397303880a171c3323a432b9771802c546c
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-26 13:00:07 -0400

    started on the login page

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,24 @@ console.log('crazy app.js');
 
 const app = angular.module('CrazyApp', []);
 
+const url= "https://localhost:3000";
 
 app.controller('MainController', ['$http', function($http){
+   this.showHomePage = false;
+   this.showBuildsPage = false;
+   this.showLoginPage = true;
+   this.builds = [];
+   this.clearScreen = function(){
+      this.showHomePage = false;
+      this.showBuildsPage = false;
+      this.showLoginPage = false;
+   };
 
-
+   // $http({
+   //    method:"GET",
+   //    url: url + '/builds'
+   // }).then(function(response){
+   //    console.log(response.data);
+   // });
 
 }]);
