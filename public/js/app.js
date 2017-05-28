ba2c88a84c19086269f395890b04a34b74fa83af
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-27 22:54:21 -0400

    data is now showing up on the build page

--- public/js/app.js
+++ public/js/app.js
@@ -2,24 +2,55 @@ console.log('crazy app.js');
 
 const app = angular.module('CrazyApp', []);
 
-const url= "https://localhost:3000";
+const url= "http://localhost:3000";
 
 app.controller('MainController', ['$http', function($http){
+   this.test="hi"
    this.showHomePage = false;
    this.showBuildsPage = false;
    this.showLoginPage = true;
+
+   this.toggleLoginForm = true;
+   this.toggleRegForm = false;
    this.builds = [];
+   this.user = {};
+
    this.clearScreen = function(){
       this.showHomePage = false;
       this.showBuildsPage = false;
       this.showLoginPage = false;
    };
+   this.loadRegForm = function(){
+      this.toggleLoginForm = false;
+      this.toggleRegForm = true;
+   };
+   this.loadLoginForm = function(){
+      this.toggleLoginForm = true;
+      this.toggleRegForm = false;
+   };
 
-   // $http({
-   //    method:"GET",
-   //    url: url + '/builds'
-   // }).then(function(response){
-   //    console.log(response.data);
-   // });
+
+   // USER INFORMATION
+   this.login = function(userPass) {
+      console.log(userPass);
+
+      $http({
+         method: 'POST',
+         url: url + '/users/login',
+         data: { user: { username: userPass.username, password: userPass.password }},
+      }).then(function(response) {
+         console.log(response);
+         this.user = response.data.user;
+      }.bind(this));
+   };
+
+
+   $http({
+      method:"GET",
+      url: url + '/builds',
+   }).then(function(response){
+      console.log(response);
+      this.builds = response.data
+   }.bind(this));
 
 }]);
