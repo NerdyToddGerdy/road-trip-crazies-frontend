78140404439cb0656edc2ced89c2bf1be33282a1
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-29 16:41:08 -0400

    I now have the ability to create and delete a user.

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,7 @@ app.controller('MainController', ['$http', function($http){
    this.showBuildsPage = false;
    this.showLoginPage = true;
    this.showChatPage = false;
+   this.showProfilePage = false;
 
    this.toggleLoginForm = true;
    this.toggleRegForm = false;
@@ -19,12 +20,17 @@ app.controller('MainController', ['$http', function($http){
    this.regFormData = {};
    this.builds = [];
    this.user = {};
+   this.users = [];
 
+// ---------------------------------
+// ******* START PAGE NAVIGATION ******
+// ---------------------------------
    this.clearScreen = function(){
       this.showHomePage = false;
       this.showBuildsPage = false;
       this.showLoginPage = false;
       this.showChatPage = false;
+      this.showProfilePage = false;
    };
    this.loadRegForm = function(){
       this.toggleLoginForm = false;
@@ -34,8 +40,15 @@ app.controller('MainController', ['$http', function($http){
       this.toggleLoginForm = true;
       this.toggleRegForm = false;
    };
+// ---------------------------------
+// ****** END PAGE NAVIGATION ******
+// ---------------------------------
 
-   // Registration form -- needs to sign in after creation
+
+
+// ---------------------------------
+// ****** USERS ******
+// ---------------------------------
    this.register = function(){
       console.log("regFormData ",this.regformdata);
       $http({
@@ -49,13 +62,13 @@ app.controller('MainController', ['$http', function($http){
          }
       }).then(function(response){
          console.log(response);
+         this.login(response.data);
       }.bind(this));
    };
 
-   // USER INFORMATION
+   // THIS USER INFORMATION
    this.login = function(userPass) {
       console.log(userPass);
-
       $http({
          method: 'POST',
          url: url + '/users/login',
@@ -63,8 +76,37 @@ app.controller('MainController', ['$http', function($http){
       }).then(function(response) {
          console.log(response);
          this.user = response.data.user;
+         this.clearScreen();
+         this.showHomePage = true;
       }.bind(this));
    };
+
+// DELETE SELECTED USER
+   this.deleteThisUser = function(person){
+      console.log(person);
+      $http({
+         method: 'DELETE',
+         url: url + '/users/' + person.id
+      }).then(function(response){
+         console.log(response);
+         this.findAllUsers();
+      }.bind(this));
+   };
+   // FIND ALL USERS
+   this.findAllUsers = function(){
+      $http({
+         method: "GET",
+         url: url + '/users'
+      }).then(function(response){
+         this.users = response.data;
+         console.log(this.users);
+      }.bind(this));
+   };
+   this.findAllUsers();
+// ---------------------------------
+// ****** USERS ******
+// ---------------------------------
+
 
 
    $http({
