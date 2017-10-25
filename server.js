49b1a4d7a3e7f6ef6e1ba68164f6dde24020ca18
Todd Gerdy <toadimous@gmail.com>
2017-10-24 21:30:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cloudinary = require('cloudinary')
+const cloudinary = require("cloudinary");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 cloudinary.config({
-   cloud_name: 'toddles',
-   api_key: '464326275517746',
-   api_secret: 'SGFxqvO4lzuxU8GoBl4rVf7gkZc'
+   cloud_name: "toddles",
+   api_key: "464326275517746",
+   api_secret: "SGFxqvO4lzuxU8GoBl4rVf7gkZc"
 });
 
 // cloudinary.uploader.upload("./public/images/Tom.jpg", function(result) {
@@ -16,4 +16,4 @@ cloudinary.config({
 
 const port = process.env.PORT || 4040;
 
-app.listen(port, () => console.log('running on port: ', port));
+app.listen(port, () => console.log("running on port: ", port));
